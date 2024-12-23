bc1bac28fac7b768c0df0fad81f5b911d07532ed
SCOTTGeek-dev <ham.youness@gmail.com>
2024-12-23 01:53:25 +0100

    circuling borders fix

--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -11,28 +11,42 @@ class _OnboardingState extends State<Onboarding> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      child: Stack(
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            child: Image.asset(
-              "images/background.jpg",
-              fit: BoxFit.cover,
+      body: Container(
+        child: Stack(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Image.asset(
+                "images/background.jpg",
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          Center(child: Image.asset("images/logo.png")),
-          Container(
-            width: MediaQuery.of(context).size.width / 2,
-            decoration: BoxDecoration(),
-            child: Text(
-              "Commencer à jouer",
-              style: TextStyle(color: Colors.black),
-            ),
-          )
-        ],
+            Center(child: Image.asset("images/logo.png")),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  height: 50,
+                  width: MediaQuery.of(context).size.width / 2,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30)),
+                  child: Center(
+                    child: Text(
+                      "Commencer à jouer",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
